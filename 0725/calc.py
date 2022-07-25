2f38b8ba59c3c04ff1228b2c06e18b78a9616f66
Sungwook Cho <sungwookcho@Sungwookui-MacBookPro.local>
2022-07-25 21:13:26 +0900

    알고리즘 기차 풀이

---
+++ 0725/calc.py
@@ -0,0 +1,18 @@
+
+
+
+def add(a,b):
+    return a + b
+
+def sub(a,b):
+    return a - b
+
+def mul(a,b):
+    return a * b
+
+def div(a,b):
+
+    if  b:
+        return a / b
+    else:
+        print("0으로 나눌 수 없습니다")
---
+++ 0725/ws5_1.py
@@ -0,0 +1,23 @@
+
+
+
+fruits = 'apple,rottenBanana,apple,RoTTenorange,Orange'
+
+# 함수 사용해서 만들어보기
+fruits_list = list(fruits.split(','))     # 문자열이 ','로 나누어져 있으므로 split(',') 함수를 통해 리스트 형태로 만들기
+
+
+newFruits_list = []
+for fruit in fruits_list:
+    lowerfruit = fruit.lower()  # 소문자로 바꾸는 메서드 .lower() - lower()함수는 원본을 건드리지 않고 새로운 문자열을 생성한다
+    if lowerfruit[:6] == 'rotten':
+        newFruits_list.append(lowerfruit[6:])  # 슬라이싱[] - 얕은복사: 원본에 영향을 주지 않는다.
+        
+    else:
+        newFruits_list.append(lowerfruit)
+    
+    
+print(newFruits_list)
+
+
+# for 문을 도는 literable 리스트의 값을 insert() 함수로 변환시켰기에 무한루프가 돌았다
